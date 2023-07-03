9c066bbbd5846be9545c326e09f16a833a8f3343
AbbasNaqvi10 <syedabbasnaqvi10@gmail.com>
2023-07-03 13:13:25 +0500

    yarn config change forking mainnet

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.9.1) (governance/Governor.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.RPC_URL as string,
+      },
+    },
+    mainnet: {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONMUMBAISCAN_API_KEY,
+  },
 };
 
 export default config;
